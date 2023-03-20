fdf4184cd8fe4f9c8388bcc480227e7acfc3f864
xipeitao <xipeitao@yy.com>
2023-03-21 00:21:09 +0800

    homelist

--- android/app/src/main/java/com/eathemeat/easytimer/ui/fragment/HomeFragment.kt
+++ android/app/src/main/java/com/eathemeat/easytimer/ui/fragment/HomeFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Orientation
 import com.eathemeat.easytimer.R
 import com.eathemeat.easytimer.databinding.ActivityHomeBinding
 import com.eathemeat.easytimer.databinding.FragmentHomeBinding
@@ -19,19 +23,42 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
     private lateinit var binding: FragmentHomeBinding
 
+    private lateinit var mLayoutManager:LinearLayoutManager
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        //recycleview
+        mLayoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL,false)
+
+
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
     }
+
+    class HomeListAdapter: RecyclerView.Adapter<ViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(parent,viewType)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+    }
+
 
 }
