58fd011ae3ea8fdf69afc418c782b07ebbc7cdb8
PeterXi <peter12757@126.com>
2024-08-26 17:59:16 +0800

    new

---
+++ android/app/src/main/java/com/eathemeat/easytimer/util/AlertWinManager.kt
@@ -0,0 +1,16 @@
+package com.eathemeat.easytimer.util
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+
+class AlertWinManager {
+
+    fun checkPermission(context: Context): Boolean {
+
+    }
+}
