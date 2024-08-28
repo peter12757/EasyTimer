ac9adea3e4b9385ef6a463733a9d2fa764224691
PeterXi <peter12757@126.com>
2024-08-28 10:07:21 +0800

    new project

--- android/app/src/main/java/com/eathemeat/easytimer/ui/fragment/AddViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.eathemeat.easytimer.ui.fragment
-
-import androidx.lifecycle.ViewModel
-
-class AddViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- android/app/src/main/java/com/eathemeat/easytimer/ui/fragment/DoneViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.eathemeat.easytimer.ui.fragment
-
-import androidx.lifecycle.ViewModel
-
-class DoneViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
