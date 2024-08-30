e28c8d51773fa88a2f1d3317334e1e7d3a8ba530
PeterXi <peter12757@126.com>
2024-08-30 10:41:49 +0800

    add alert

---
+++ android/app/src/main/java/com/eathemeat/easytimer/AlertManager.kt
@@ -0,0 +1,7 @@
+package com.eathemeat.easytimer
+
+object AlertManager {
+
+
+
+}
--- android/app/src/main/java/com/eathemeat/easytimer/page/AddPage.kt
+++ android/app/src/main/java/com/eathemeat/easytimer/page/AddPage.kt
@@ -1,12 +1,18 @@
 package com.eathemeat.easytimer.page
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 
 @Composable
 fun AddPage() {
-    Text(text = "AddPage")
+    ConstraintLayout {
+        Column {
+
+        }
+    }
 }
 
 @Preview
